3e137de361814fe6e241e80121c9a59a21b5d10c
seamus0192 <seamus0192@gmail.com>
2022-11-30 22:15:13 -0800

    sauce time

---
+++ src/SauceEntity.java
@@ -0,0 +1,10 @@
+import processing.core.PImage;
+
+import java.util.*;
+
+public class SauceEntity extends Entity{
+    public SauceEntity(String id, Point position, List<PImage> images) {
+        super( id, position, images);
+    }
+
+}
--- src/VirtualWorld.java
+++ src/VirtualWorld.java
@@ -45,6 +45,7 @@ public final class VirtualWorld extends PApplet
     private TimerTask task;
     private TimerTask task2;
     private TimerTask task3;
+    private TimerTask task4;
 
     public static MicrowaveEntity Microwave;
 
@@ -88,13 +89,22 @@ public final class VirtualWorld extends PApplet
                 world.addEntity(f);
             }
         };
+        task4 = new TimerTask() {
+            public void run() {
+                SauceEntity f = new SauceEntity("saucy", findNewRandomPoint(),imageStore.getImageList("sauce"));
+                world.addEntity(f);
+            }
+        };
         //multiple to fix concurrent modification errors
         Timer t = new Timer();
         Timer t1 = new Timer();
         Timer t2 = new Timer();
+        Timer t3 = new Timer();
         t.scheduleAtFixedRate(task, 10000,20000);
-        t1.scheduleAtFixedRate(task2, 2000,5001);
+        t1.scheduleAtFixedRate(task2, 2001,5001);
         t2.scheduleAtFixedRate(task3, 2000,10003);
+        t3.scheduleAtFixedRate(task4, 20002,40002);
+
 
 
         loadImages(IMAGE_LIST_FILE_NAME, imageStore, this);
@@ -125,21 +135,27 @@ public final class VirtualWorld extends PApplet
     }
 
     public void draw() {
-        long time = System.currentTimeMillis();
-        if (time >= nextTime) {
-            this.scheduler.updateOnTime(time);
-            nextTime = time + TIMER_ACTION_PERIOD;
-        }
+        try {
+            long time = System.currentTimeMillis();
+            if (time >= nextTime) {
+                this.scheduler.updateOnTime(time);
+                nextTime = time + TIMER_ACTION_PERIOD;
+            }
 
-        view.drawViewport();
-        textSize(20);
-        text("Kills:" + ProjectileEntity.kills,10,25);
-        text("Health:" + LasagnaEntity.health,10,60);
-        text("Points:" + LasagnaEntity.cooks, 1010,25);
-        if (LasagnaEntity.health == 0){
-            textSize(60);
-            text("GAME OVER", 300,500);
-            stop();
+            view.drawViewport();
+            textSize(20);
+            text("Kills:" + ProjectileEntity.kills, 10, 25);
+            text("Health:" + LasagnaEntity.health, 10, 60);
+            text("Points:" + LasagnaEntity.cooks, 1010, 25);
+            if (LasagnaEntity.health == 0) {
+                fill(255,0,0);
+                textSize(60);
+                text("GAME OVER", 350, 450);
+                stop();
+            }
+        } catch (ConcurrentModificationException e)
+        {
+            draw();
         }
     }
 
